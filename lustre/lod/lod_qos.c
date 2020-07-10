LaudateCorpus1/lustre

commit d9d149835401565812dd85f3fe6be7b25b0709e8

LUS-8157 lod: check for extension size at instantiation time
lod_statfs_and_check() may consider an OST as OK, but SEL needs its
extension size to fit the free space and the OST may turn out to be
low-on-space, thus inappropriate OST may be chosen, SEL cannot use
it and ENOSPC occurs.

Take the extension size into account at the object allocation time.
However, if none of OSTs is able to alloca an object, try to allocate
in old manner insteadof returning ENOSPC from lod_qos_prep_create().

Adjust tests to work on smaller extension sizes to fit into OSTs
used for sanity tests to enable to new logic above.

Signed-off-by: Vitaly Fertman <c17818@cray.com>
Change-Id: If5159200a72a68a7261aa7031e58a1ac6a8e3f24
Reviewed-by: Alexey Lyashkov <c17817@cray.com>
Reviewed-by: Andriy Skulysh <c17819@cray.com>
Tested-by: Elena Gryaznova <c17455@cray.com>
Reviewed-on: https://es-gerrit.dev.cray.com/156372

lustre/lod/lod_qos.c
@@ -193,8 +193,9 @@ out:
 
  */
 static int lod_statfs_and_check(const struct lu_env *env, struct lod_device *d,
-				int index, struct obd_statfs *sfs)
+				int index, struct obd_statfs *sfs, __u64 reserve)
 {
+	struct obd_statfs_info info = { 0 };
 	struct lod_tgt_desc *ost;
 	int		     rc;
 	ENTRY;
@@ -203,10 +204,15 @@ static int lod_statfs_and_check(const struct lu_env *env, struct lod_device *d,
 	ost = OST_TGT(d,index);
 	LASSERT(ost);
 
-	rc = dt_statfs(env, ost->ltd_ost, sfs);
+	rc = dt_statfs_info(env, ost->ltd_ost, sfs, &info);
 
 	if (rc == 0 && ((sfs->os_state & OS_STATE_ENOSPC) ||
 	    (sfs->os_state & OS_STATE_ENOINO && sfs->os_fprecreated == 0)))
+		RETURN(-ENOSPC);
+
+	if (reserve &&
+	    (reserve + (info.os_reserved_mb_low << 20) >
+	     sfs->os_bavail * sfs->os_bsize))
 		RETURN(-ENOSPC);
 
 	if (rc && rc != -ENOTCONN)
@@ -293,7 +299,7 @@ void lod_qos_statfs_update(const struct lu_env *env, struct lod_device *lod)
 		idx = osts->op_array[i];
 		avail = OST_TGT(lod,idx)->ltd_statfs.os_bavail;
 		if (lod_statfs_and_check(env, lod, idx,
-					 &OST_TGT(lod, idx)->ltd_statfs))
+					 &OST_TGT(lod, idx)->ltd_statfs, 0))
 			continue;
 		if (OST_TGT(lod,idx)->ltd_statfs.os_bavail != avail)
 			/* recalculate weigths */
@@ -940,7 +946,8 @@ static int lod_check_and_reserve_ost(const struct lu_env *env,
 				     __u32 speed, __u32 *s_idx,
 				     struct dt_object **stripe,
 				     __u32 *ost_indices,
-				     struct thandle *th)
+				     struct thandle *th,
+				     __u64 reserve)
 {
 	struct lod_device *lod = lu2lod_dev(lo->ldo_obj.do_lu.lo_dev);
 	struct lod_avoid_guide *lag = &lod_env_info(env)->lti_avoid;
@@ -949,7 +956,7 @@ static int lod_check_and_reserve_ost(const struct lu_env *env,
 	int rc;
 	ENTRY;
 
-	rc = lod_statfs_and_check(env, lod, ost_idx, sfs);
+	rc = lod_statfs_and_check(env, lod, ost_idx, sfs, reserve);
 	if (rc)
 		RETURN(rc);
 
@@ -1045,7 +1052,7 @@ static int lod_check_and_reserve_ost(const struct lu_env *env,
  */
 static int lod_alloc_rr(const struct lu_env *env, struct lod_object *lo,
 			struct dt_object **stripe, __u32 *ost_indices,
-			int flags, struct thandle *th, int comp_idx)
+			int flags, struct thandle *th, int comp_idx, __u64 reserve)
 {
 	struct lod_layout_component *lod_comp;
 	struct lod_device *m = lu2lod_dev(lo->ldo_obj.do_lu.lo_dev);
@@ -1133,7 +1140,7 @@ repeat_find:
 		spin_unlock(&lqr->lqr_alloc);
 		rc = lod_check_and_reserve_ost(env, lo, sfs, ost_idx, speed,
 					       &stripe_idx, stripe, ost_indices,
-					       th);
+					       th, reserve);
 		spin_lock(&lqr->lqr_alloc);
 
 		if (rc != 0 && OST_TGT(m, ost_idx)->ltd_connecting)
@@ -1200,7 +1207,7 @@ out:
  */
 static int lod_alloc_ost_list(const struct lu_env *env, struct lod_object *lo,
 			      struct dt_object **stripe, __u32 *ost_indices,
-			      struct thandle *th, int comp_idx)
+			      struct thandle *th, int comp_idx, __u64 reserve)
 {
 	struct lod_layout_component *lod_comp;
 	struct lod_device	*m = lu2lod_dev(lo->ldo_obj.do_lu.lo_dev);
@@ -1257,7 +1264,7 @@ static int lod_alloc_ost_list(const struct lu_env *env, struct lod_object *lo,
 			break;
 		}
 
-		rc = lod_statfs_and_check(env, m, ost_idx, sfs);
+		rc = lod_statfs_and_check(env, m, ost_idx, sfs, reserve);
 		if (rc < 0) /* this OSP doesn't feel well */
 			break;
 
@@ -1310,7 +1317,8 @@ static int lod_alloc_ost_list(const struct lu_env *env, struct lod_object *lo,
  */
 static int lod_alloc_specific(const struct lu_env *env, struct lod_object *lo,
 			      struct dt_object **stripe, __u32 *ost_indices,
-			      int flags, struct thandle *th, int comp_idx)
+			      int flags, struct thandle *th, int comp_idx,
+			      __u64 reserve)
 {
 	struct lod_layout_component *lod_comp;
 	struct lod_device *m = lu2lod_dev(lo->ldo_obj.do_lu.lo_dev);
@@ -1390,7 +1398,7 @@ repeat_find:
 		 * start OST, then it can be skipped, otherwise skip it only
 		 * if it is inactive/recovering/out-of-space." */
 
-		rc = lod_statfs_and_check(env, m, ost_idx, sfs);
+		rc = lod_statfs_and_check(env, m, ost_idx, sfs, reserve);
 		if (rc) {
 			/* this OSP doesn't feel well */
 			continue;
@@ -1515,7 +1523,7 @@ static inline int lod_qos_is_usable(struct lod_device *lod)
  */
 static int lod_alloc_qos(const struct lu_env *env, struct lod_object *lo,
 			 struct dt_object **stripe, __u32 *ost_indices,
-			 int flags, struct thandle *th, int comp_idx)
+			 int flags, struct thandle *th, int comp_idx, __u64 reserve)
 {
 	struct lod_layout_component *lod_comp;
 	struct lod_device *lod = lu2lod_dev(lo->ldo_obj.do_lu.lo_dev);
@@ -1579,7 +1587,7 @@ static int lod_alloc_qos(const struct lu_env *env, struct lod_object *lo,
 		ost = OST_TGT(lod, osts->op_array[i]);
 		ost->ltd_qos.ltq_usable = 0;
 
-		rc = lod_statfs_and_check(env, lod, osts->op_array[i], sfs);
+		rc = lod_statfs_and_check(env, lod, osts->op_array[i], sfs, reserve);
 		if (rc) {
 			/* this OSP doesn't feel well */
 			continue;
@@ -2288,7 +2296,7 @@ void lod_collect_avoidance(struct lod_object *lo, struct lod_avoid_guide *lag,
  */
 int lod_qos_prep_create(const struct lu_env *env, struct lod_object *lo,
 			struct lu_attr *attr, struct thandle *th,
-			int comp_idx)
+			int comp_idx, __u64 reserve)
 {
 	struct lod_layout_component *lod_comp;
 	struct lod_device      *d = lu2lod_dev(lod2lu_obj(lo)->lo_dev);
@@ -2335,6 +2343,7 @@ int lod_qos_prep_create(const struct lu_env *env, struct lod_object *lo,
 		if (!ost_indices)
 			GOTO(out, rc = -ENOMEM);
 
+repeat:
 		lod_getref(&d->lod_ost_descs);
 		/* XXX: support for non-0 files w/o objects */
 		CDEBUG(D_OTHER, "tgt_count %d stripe_count %d\n",
@@ -2343,7 +2352,7 @@ int lod_qos_prep_create(const struct lu_env *env, struct lod_object *lo,
 		if (lod_comp->llc_ostlist.op_array &&
 		    lod_comp->llc_ostlist.op_count) {
 			rc = lod_alloc_ost_list(env, lo, stripe, ost_indices,
-						th, comp_idx);
+						th, comp_idx, reserve);
 		} else if (lod_comp->llc_stripe_offset == LOV_OFFSET_DEFAULT) {
 			/**
 			 * collect OSTs and OSSs used in other mirrors whose
@@ -2358,13 +2367,13 @@ int lod_qos_prep_create(const struct lu_env *env, struct lod_object *lo,
 			lod_collect_avoidance(lo, lag, comp_idx);
 
 			rc = lod_alloc_qos(env, lo, stripe, ost_indices, flag,
-					   th, comp_idx);
+					   th, comp_idx, reserve);
 			if (rc == -EAGAIN)
 				rc = lod_alloc_rr(env, lo, stripe, ost_indices,
-						  flag, th, comp_idx);
+						  flag, th, comp_idx, reserve);
 		} else {
 			rc = lod_alloc_specific(env, lo, stripe, ost_indices,
-						flag, th, comp_idx);
+						flag, th, comp_idx, reserve);
 		}
 put_ldts:
 		lod_putref(d, &d->lod_ost_descs);
@@ -2372,6 +2381,15 @@ put_ldts:
 			for (i = 0; i < stripe_len; i++)
 				if (stripe[i] != NULL)
 					dt_object_put(env, stripe[i]);
+
+			/* In case there is no space on any OST, let's ignore
+			 * the @reserve space to avoid an error at the init
+			 * time, probably the actual IO will be less than the
+			 * given @reserve space (aka extension_size). */
+			if (reserve) {
+				reserve = 0;
+				goto repeat;
+			}
 			lod_comp->llc_stripe_count = 0;
 		} else {
 			lod_comp->llc_stripe = stripe;
@@ -2473,7 +2491,7 @@ int lod_prepare_create(const struct lu_env *env, struct lod_object *lo,
 		}
 
 		if (!lo->ldo_is_composite || size >= extent->e_start) {
-			rc = lod_qos_prep_create(env, lo, attr, th, i);
+			rc = lod_qos_prep_create(env, lo, attr, th, i, 0);
 			if (rc)
 				break;
 		}

lustre/lod/lod_internal.h
@@ -744,7 +744,7 @@ int lod_qos_parse_config(const struct lu_env *env, struct lod_object *lo,
 			 const struct lu_buf *buf);
 int lod_qos_prep_create(const struct lu_env *env, struct lod_object *lo,
 			struct lu_attr *attr, struct thandle *th,
-			int comp_idx);
+			int comp_idx, __u64 reserve);
 __u16 lod_comp_entry_stripe_count(struct lod_object *lo,
 				  struct lod_layout_component *entry,
 				  bool is_dir);

lustre/lod/lod_object.c
@@ -4283,6 +4283,7 @@ static int lod_layout_repeat_comp(const struct lu_env *env,
 	new_comp->llc_flags &= ~LCME_FL_INIT;
 	new_comp->llc_stripe = NULL;
 	new_comp->llc_stripes_allocated = 0;
+	new_comp->llc_ost_indices = NULL;
 	new_comp->llc_stripe_offset = LOV_OFFSET_DEFAULT;
 	/* for uninstantiated components, layout gen stores default stripe
 	 * offset */
@@ -6187,7 +6188,9 @@ static int lod_invalidate(const struct lu_env *env, struct dt_object *dt)
 }
 
 static int lod_declare_instantiate_components(const struct lu_env *env,
-		struct lod_object *lo, struct thandle *th)
+					      struct lod_object *lo,
+					      struct thandle *th,
+					      __u64 reserve)
 {
 	struct lod_thread_info *info = lod_env_info(env);
 	int i;
@@ -6198,7 +6201,7 @@ static int lod_declare_instantiate_components(const struct lu_env *env,
 
 	for (i = 0; i < info->lti_count; i++) {
 		rc = lod_qos_prep_create(env, lo, NULL, th,
-					 info->lti_comp_idx[i]);
+					 info->lti_comp_idx[i], reserve);
 		if (rc)
 			break;
 	}
@@ -6234,36 +6237,21 @@ static int lod_declare_instantiate_components(const struct lu_env *env,
  */
 static bool lod_sel_osts_allowed(const struct lu_env *env,
 				 struct lod_object *lo,
-				 int index, __u64 extension_size,
+				 int index, __u64 reserve,
 				 struct lu_extent *extent,
 				 struct lu_extent *comp_extent, int write)
 {
 	struct lod_layout_component *lod_comp = &lo->ldo_comp_entries[index];
 	struct lod_device *lod = lu2lod_dev(lo->ldo_obj.do_lu.lo_dev);
-	struct obd_statfs *sfs = &lod_env_info(env)->lti_osfs;
+	struct lod_thread_info *tinfo = lod_env_info(env);
+	struct obd_statfs *sfs = &tinfo->lti_osfs;
 	__u64 available = 0;
-	__u64 size;
 	bool ret = true;
 	int i, rc;
 
 	ENTRY;
 
 	LASSERT(lod_comp->llc_stripe_count != 0);
-
-	if (write == 0 ||
-	    (extent->e_start == 0 && extent->e_end == OBD_OBJECT_EOF)) {
-		/* truncate or append */
-		size = extension_size;
-	} else {
-		/* In case of write op, check the real write extent,
-		 * it may be larger than the extension_size */
-		size = roundup(min(extent->e_end, comp_extent->e_end) -
-			       max(extent->e_start, comp_extent->e_start),
-			       extension_size);
-	}
-	/* extension_size is file level, so we must divide by stripe count to
-	 * compare it to available space on a single OST */
-	size /= lod_comp->llc_stripe_count;
 
 	lod_getref(&lod->lod_ost_descs);
 	for (i = 0; i < lod_comp->llc_stripe_count; i++) {
@@ -6325,11 +6313,11 @@ static bool lod_sel_osts_allowed(const struct lu_env *env,
 		       (100ull * sfs->os_bavail) / sfs->os_blocks,
 		       (100ull * sfs->os_bfree) / sfs->os_blocks);
 
-		if (size * repeated > available) {
+		if (reserve * repeated > available) {
 			ret = false;
 			CDEBUG(D_LAYOUT, "low space on ost %d, available %llu "
-			       "< extension size %llu\n", index, available,
-			       extension_size);
+			       "< extension size %llu repeated %d\n", index, available,
+			       reserve, repeated);
 			break;
 		}
 	}
@@ -6435,6 +6423,25 @@ static __u64 lod_extension_new_end(__u64 extension_size, __u64 extent_end,
 	return new_end;
 }
 
+/**
+ * Calculate the exact reservation (per-OST extension_size) on the OSTs being
+ * instantiated. It needs to be calculated in advance and taken into account at the
+ * instantiation time, because otherwise lod_statfs_and_check() may consider an OST
+ * as OK, but SEL needs its extension_size to fit the free space and the OST may turn
+ * out to be low-on-space, thus inappropriate OST may be used and ENOSPC occurs.
+ *
+ * \param[in] lod_comp		lod component we are checking
+ *
+ * \retval	size to reserved on each OST of lod_comp's stripe.
+ */
+static __u64 lod_sel_stripe_reserved(struct lod_layout_component *lod_comp)
+{
+	/* extension_size is file level, so we must divide by stripe count to
+	 * compare it to available space on a single OST */
+	return  lod_comp->llc_stripe_size * SEL_UNIT_SIZE /
+		lod_comp->llc_stripe_count;
+}
+
 /* As lod_sel_handler() could be re-entered for the same component several
  * times, this is the data for the next call. Fields could be changed to
  * component indexes when needed, (e.g. if there is no need to instantiate
@@ -6516,7 +6523,7 @@ static int lod_sel_handler(const struct lu_env *env,
 	struct lod_layout_component *lod_comp;
 	struct lod_layout_component *prev;
 	struct lod_layout_component *next = NULL;
-	__u64 extension_size;
+	__u64 extension_size, reserve;
 	__u64 new_end = 0;
 	bool repeated;
 	int change = 0;
@@ -6553,11 +6560,13 @@ static int lod_sel_handler(const struct lu_env *env,
 		RETURN(-EINVAL);
 	}
 
+	reserve = lod_sel_stripe_reserved(lod_comp);
+
 	if (!prev->llc_stripe) {
 		CDEBUG(D_LAYOUT, "Previous component not inited\n");
 		info->lti_count = 1;
 		info->lti_comp_idx[0] = index - 1;
-		rc = lod_declare_instantiate_components(env, lo, th);
+		rc = lod_declare_instantiate_components(env, lo, th, reserve);
 		/* ENOSPC tells us we can't use this component.  If there is
 		 * a next or we are repeating, we either spill over (next) or
 		 * extend the original comp (repeat).  Otherwise, return the
@@ -6569,8 +6578,7 @@ static int lod_sel_handler(const struct lu_env *env,
 	}
 
 	if (sd->sd_force == 0 && rc == 0)
-		rc = !lod_sel_osts_allowed(env, lo, index - 1,
-					   extension_size, extent,
+		rc = !lod_sel_osts_allowed(env, lo, index - 1, reserve, extent,
 					   &lod_comp->llc_extent, write);
 
 	repeated = !!(sd->sd_repeat);
@@ -6884,7 +6892,7 @@ static int lod_declare_update_plain(const struct lu_env *env,
 		RETURN(-EALREADY);
 
 	lod_obj_inc_layout_gen(lo);
-	rc = lod_declare_instantiate_components(env, lo, th);
+	rc = lod_declare_instantiate_components(env, lo, th, 0);
 	EXIT;
 out:
 	if (rc)
@@ -7301,7 +7309,7 @@ static int lod_declare_update_rdonly(const struct lu_env *env,
 		lo->ldo_layout_gen = layout_version & 0xffff;
 	}
 
-	rc = lod_declare_instantiate_components(env, lo, th);
+	rc = lod_declare_instantiate_components(env, lo, th, 0);
 	if (rc)
 		GOTO(out, rc);
 
@@ -7445,7 +7453,7 @@ static int lod_declare_update_write_pending(const struct lu_env *env,
 		lo->ldo_flr_state = LCM_FL_SYNC_PENDING;
 	}
 
-	rc = lod_declare_instantiate_components(env, lo, th);
+	rc = lod_declare_instantiate_components(env, lo, th, 0);
 	if (rc)
 		GOTO(out, rc);
 

lustre/osp/osp_dev.c
@@ -754,12 +754,11 @@ static int osp_statfs(const struct lu_env *env, struct dt_device *dev,
 		RETURN(0);
 
 	CDEBUG(D_OTHER, "%s: %llu blocks, %llu free, %llu avail, "
-	       "%u reserved mb low, %u reserved mb high,"
+	       "%u bsize, %u reserved mb low, %u reserved mb high,"
 	       "%llu files, %llu free files\n", d->opd_obd->obd_name,
-	       sfs->os_blocks, sfs->os_bfree, sfs->os_bavail,
+	       sfs->os_blocks, sfs->os_bfree, sfs->os_bavail, sfs->os_bsize,
 	       d->opd_reserved_mb_low, d->opd_reserved_mb_high,
 	       sfs->os_files, sfs->os_ffree);
-
 
 	if (info && !info->os_enable_pre)
 		RETURN(0);

lustre/osp/osp_precreate.c
@@ -119,6 +119,7 @@ static int osp_statfs_interpret(const struct lu_env *env,
 {
 	struct obd_import *imp = req->rq_import;
 	struct obd_statfs *msfs;
+	struct obd_statfs *sfs;
 	struct osp_device *d;
 	u64 maxage_ns;
 
@@ -147,7 +148,13 @@ static int osp_statfs_interpret(const struct lu_env *env,
 		  jiffies + cfs_time_seconds(d->opd_statfs_maxage));
 	d->opd_statfs_update_in_progress = 0;
 
-	CDEBUG(D_CACHE, "updated statfs %p\n", d);
+	sfs = &d->opd_statfs;
+	CDEBUG(D_CACHE, "%s (%p): %llu blocks, %llu free, %llu avail, "
+	       "%u bsize, %u reserved mb low, %u reserved mb high,"
+	       "%llu files, %llu free files\n", d->opd_obd->obd_name, d,
+	       sfs->os_blocks, sfs->os_bfree, sfs->os_bavail, sfs->os_bsize,
+	       d->opd_reserved_mb_low, d->opd_reserved_mb_high,
+	       sfs->os_files, sfs->os_ffree);
 
 	RETURN(0);
 out:

lustre/tests/sanity-pfl.sh
@@ -1236,7 +1236,7 @@ test_20a() {
 	wait_mds_ost_sync
 
 	# First component is on OST0
-	$LFS setstripe -E 256M -i 0 -z 64M -E -1 -z 1G $comp_file ||
+	$LFS setstripe -E 256M -i 0 -z 64M -E -1 -z 128M $comp_file ||
 		error "Create $comp_file failed"
 
 	# write past end of first component, so it is extended
@@ -1260,12 +1260,12 @@ test_20a() {
 	$LFS getstripe $comp_file
 
 	flg_opts="--comp-flags init"
-	found=$($LFS find --comp-start 128M -E 1152M $flg_opts $comp_file | \
+	found=$($LFS find --comp-start 128M -E 256M $flg_opts $comp_file | \
 		wc -l)
 	[ $found -eq 1 ] || error "Write: third component not found"
 
 	flg_opts="--comp-flags extension"
-	found=$($LFS find --comp-start 1152M -E EOF $flg_opts $comp_file |wc -l)
+	found=$($LFS find --comp-start 256M -E EOF $flg_opts $comp_file |wc -l)
 	[ $found -eq 1 ] || error "Write: fourth extension component not found"
 
 	sel_layout_sanity $comp_file 3
@@ -1289,7 +1289,7 @@ test_20b() {
 
 	# normal component to 10M, extendable component to 1G
 	# further extendable to EOF
-	$LFS setstripe -E 10M -E 1G -p $TESTNAME -z 64M -E -1 -p "" -z 512M \
+	$LFS setstripe -E 10M -E 1G -p $TESTNAME -z 64M -E -1 -p "" -z 128M \
 		$comp_file || error "Create $comp_file failed"
 
 	replay_barrier $SINGLEMDS
@@ -1314,11 +1314,11 @@ test_20b() {
 	[ $found -eq 0 ] || error "Write: zero length component still present"
 
 	flg_opts="--comp-flags init"
-	found=$($LFS find --comp-start 10M -E 522M $flg_opts $comp_file | wc -l)
+	found=$($LFS find --comp-start 10M -E 138M $flg_opts $comp_file | wc -l)
 	[ $found -eq 1 ] || error "Write: second component not found"
 
 	flg_opts="--comp-flags extension"
-	found=$($LFS find --comp-start 522M -E EOF $flg_opts $comp_file | wc -l)
+	found=$($LFS find --comp-start 138M -E EOF $flg_opts $comp_file | wc -l)
 	[ $found -eq 1 ] || error "Write: third component not found"
 
 	fail $SINGLEMDS
@@ -1327,11 +1327,11 @@ test_20b() {
 	[ $found -eq 0 ] || error "Failover: 0-length component still present"
 
 	flg_opts="--comp-flags init"
-	found=$($LFS find --comp-start 10M -E 522M $flg_opts $comp_file | wc -l)
+	found=$($LFS find --comp-start 10M -E 138M $flg_opts $comp_file | wc -l)
 	[ $found -eq 1 ] || error "Failover: second component not found"
 
 	flg_opts="--comp-flags extension"
-	found=$($LFS find --comp-start 522M -E EOF $flg_opts $comp_file | wc -l)
+	found=$($LFS find --comp-start 138M -E EOF $flg_opts $comp_file | wc -l)
 	[ $found -eq 1 ] || error "Failover: third component not found"
 
 	sel_layout_sanity $comp_file 3
@@ -1518,7 +1518,7 @@ test_21b() {
 	test_mkdir -p $DIR/$tdir
 
 	# DoM, extendable component, further extendable component
-	$LFS setstripe -E 1M -L mdt -E 256M -i 0 -z 64M -E -1 -z 1G \
+	$LFS setstripe -E 1M -L mdt -E 256M -i 0 -z 64M -E -1 -z 128M \
 		$comp_file || error "Create $comp_file failed"
 
 	found=$($LFS find --comp-start 1M -E 1M $flg_opts $comp_file | wc -l)
@@ -1542,11 +1542,11 @@ test_21b() {
 	[ $found -eq 0 ] || error "Write: Zero length component still present"
 
 	flg_opts="--comp-flags init"
-	found=$($LFS find --comp-start 1M -E 1025M $flg_opts $comp_file | wc -l)
+	found=$($LFS find --comp-start 1M -E 129M $flg_opts $comp_file | wc -l)
 	[ $found -eq 1 ] || error "Write: extended component not found"
 
 	flg_opts="--comp-flags extension"
-	found=$($LFS find --comp-start 1025M -E EOF $flg_opts $comp_file |wc -l)
+	found=$($LFS find --comp-start 129M -E EOF $flg_opts $comp_file |wc -l)
 	[ $found -eq 1 ] || error "Write: extension component not found"
 
 	sel_layout_sanity $comp_file 3

commit a0d90b192c393689e707b8a57dd796db2f1682f2

LUS-8157 lod: lod_statfs_and_check fix
restore the os_fprecreated logic lost on the move from dt_statfs to
dt_statfs_info.

Signed-off-by: Vitaly Fertman <c17818@cray.com>
Fixes: d9d1498354 ("LUS-8157 lod: check for extension size at instantiation time")
Change-Id: I9d5c4db1f9d88a41a3264de1699424d4d14f3673
Reviewed-by: Alexey Lyashkov <c17817@cray.com>
Reviewed-by: Andriy Skulysh <c17819@cray.com>
Reviewed-on: https://es-gerrit.dev.cray.com/157462/
Tested-by: Alexander Lezhoev <c17454@cray.com>

lustre/lod/lod_qos.c
@@ -204,6 +204,7 @@ static int lod_statfs_and_check(const struct lu_env *env, struct lod_device *d,
 	ost = OST_TGT(d,index);
 	LASSERT(ost);
 
+	info.os_enable_pre = 1;
 	rc = dt_statfs_info(env, ost->ltd_ost, sfs, &info);
 
 	if (rc == 0 && ((sfs->os_state & OS_STATE_ENOSPC) ||