lustre/lustre-release

commit 71ca66bcd9c3a08edfbb67c9711c17428f599bcf

LU-13471 lnet: use the same src nid for discovery
When discovering a remote peer (not on the same network) a GET is
sent to the peer to retrieve the peer's interfaces.  This is followed
by a PUSH, if discovery is on, to push the node's interfaces However,
if both node and peer have multiple interfaces it is likely that the
GET and the PUSH will originate on different interfaces. When the
peer receives the PUSH it will not be able to connect the two NIDs
and will not be able to consolidate the node's NIDs.  This issue is
specific for remote peers because at the time the push handler is
invoked the remote lpni has not been created yet. lnet_parse()
creates the lpni of the gateway.

Similar to the strategy already in place of using the same source NID
for all the messages of an RPC, discovery should use the same source
NID for both the GET and PUSH.

This patch stores the source NID interfaces the GET was sent on and
uses it for the PUSH.

Signed-off-by: Amir Shehata <ashehata@whamcloud.com>
Change-Id: I5a13ab7799b2ddc47714202bcbed786b0d3940b7
Reviewed-on: https://review.whamcloud.com/38320
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/lnet/peer.c
@@ -258,6 +258,7 @@ lnet_peer_alloc(lnet_nid_t nid)
 	init_waitqueue_head(&lp->lp_dc_waitq);
 	spin_lock_init(&lp->lp_lock);
 	lp->lp_primary_nid = nid;
+	lp->lp_disc_src_nid = LNET_NID_ANY;
 	if (lnet_peers_start_down())
 		lp->lp_alive = false;
 	else
@@ -2291,6 +2292,8 @@ lnet_discovery_event_reply(struct lnet_peer *lp, struct lnet_event *ev)
 
 	spin_lock(&lp->lp_lock);
 
+	lp->lp_disc_src_nid = ev->target.nid;
+
 	/*
 	 * If some kind of error happened the contents of message
 	 * cannot be used. Set PING_FAILED to trigger a retry.
@@ -3109,9 +3112,17 @@ __must_hold(&lp->lp_lock)
 		goto fail_unlink;
 	}
 
-	rc = LNetPut(LNET_NID_ANY, lp->lp_push_mdh,
+	rc = LNetPut(lp->lp_disc_src_nid, lp->lp_push_mdh,
 		     LNET_ACK_REQ, id, LNET_RESERVED_PORTAL,
 		     LNET_PROTO_PING_MATCHBITS, 0, 0);
+
+	/*
+	 * reset the discovery nid. There is no need to restrict sending
+	 * from that source, if we call lnet_push_update_to_peers(). It'll
+	 * get set to a specific NID, if we initiate discovery from the
+	 * scratch
+	 */
+	lp->lp_disc_src_nid = LNET_NID_ANY;
 
 	if (rc)
 		goto fail_unlink;

lnet/include/lnet/lib-types.h
@@ -593,6 +593,9 @@ struct lnet_peer {
 	/* primary NID of the peer */
 	lnet_nid_t		lp_primary_nid;
 
+	/* source NID to use during discovery */
+	lnet_nid_t		lp_disc_src_nid;
+
 	/* net to perform discovery on */
 	__u32			lp_disc_net_id;
 

LaudateCorpus1/lustre

commit eee49bed4acacef3f54fccf5a86df72d1ded162b

LUS-9094 lnet: select reachable peer ni for discovery
Ensure the selection of a reachable peer NI.
commit 71ca66bcd9 sends the PUSH for discovery out of the same
source NID. However, this change caused an issue where a peer NI
can be selected which is not reachable from that source NID.

This patches fixes this behavior. When selecting the destination NID,
take into consideration the source NID. For directly connected peers
select a peer ni on the same net as the source NID. For routed peers
select a peer ni which has a route from the source NID.

WC-bug-id: LU-13777
Fixes: 71ca66bcd9 ("LU-13471 lnet: use the same src nid for discovery")
Signed-off-by: Amir Shehata <ashehata@whamcloud.com>
Change-Id: I7d0e0ddf5e8502a1361b2c330652a0ba9e5674cc
Reviewed-by: Alexandr Boyko <c17825@cray.com>
Reviewed-on: https://es-gerrit.dev.cray.com/157559/
Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
Tested-by: Jenkins Build User <nssreleng@cray.com>

lnet/lnet/peer.c
@@ -2989,24 +2989,38 @@ __must_hold(&lp->lp_lock)
 static lnet_nid_t lnet_peer_select_nid(struct lnet_peer *lp)
 {
 	struct lnet_peer_ni *lpni;
+	__u32 srcnet_id = LNET_NIDNET(lp->lp_disc_src_nid);
+	__u32 net_any = LNET_NIDNET(LNET_NID_ANY);
+	struct lnet_route *route;
+	struct lnet_remotenet *rnet;
+	struct lnet_peer_net *peer_net =
+		lnet_peer_get_net_locked(lp, srcnet_id);
 
 	/* Look for a direct-connected NID for this peer. */
 	lpni = NULL;
-	while ((lpni = lnet_get_next_peer_ni_locked(lp, NULL, lpni)) != NULL) {
-		if (!lnet_get_net_locked(lpni->lpni_peer_net->lpn_net_id))
-			continue;
-		break;
+	while ((lpni = lnet_get_next_peer_ni_locked(lp, peer_net, lpni)) != NULL) {
+		if (lnet_get_net_locked(lpni->lpni_peer_net->lpn_net_id))
+			goto out;
 	}
-	if (lpni)
-		return lpni->lpni_nid;
 
-	/* Look for a routed-connected NID for this peer. */
+	/*
+	 * Look for a routed-connected NID for this peer that we can reach
+	 * from the source NID if specified
+	 */
 	lpni = NULL;
 	while ((lpni = lnet_get_next_peer_ni_locked(lp, NULL, lpni)) != NULL) {
-		if (!lnet_find_rnet_locked(lpni->lpni_peer_net->lpn_net_id))
+		rnet = lnet_find_rnet_locked(lpni->lpni_peer_net->lpn_net_id);
+		if (!rnet)
 			continue;
-		break;
+		if (srcnet_id == net_any)
+			goto out;
+		list_for_each_entry(route, &rnet->lrn_routes, lr_list) {
+			if (lnet_peer_get_net_locked(route->lr_gateway, srcnet_id))
+				goto out;
+		}
 	}
+
+out:
 	if (lpni)
 		return lpni->lpni_nid;
 