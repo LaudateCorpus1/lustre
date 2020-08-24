lustre/lustre-release

commit 5db0e2de00f6b3d687fc4fe01f05a687fc74271c

LU-7403 llite: flatten struct lookup_intent
Replace the union in struct lookup_intent with the members of struct
lustre_indent_data. Remove the then unused struct lustre_intent_data.

Signed-off-by: John L. Hammond <john.hammond@intel.com>
Change-Id: I47d815f7c8e51bcff33ea7dba6efbc08337ca509
Reviewed-on: http://review.whamcloud.com/17069
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: James Simmons <uja.ornl@yahoo.com>
Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
Reviewed-by: Frank Zago <fzago@cray.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/llite/file.c
@@ -421,7 +421,7 @@ static int ll_intent_file_open(struct file *file, void *lmm, int lmmsize,
 	}
 
 	rc = ll_prep_inode(&de->d_inode, req, NULL, itp);
-	if (!rc && itp->d.lustre.it_lock_mode)
+	if (!rc && itp->it_lock_mode)
 		ll_set_lock_data(sbi->ll_md_exp, de->d_inode, itp, NULL);
 
 out:
@@ -434,13 +434,13 @@ out:
 static int ll_och_fill(struct obd_export *md_exp, struct lookup_intent *it,
 		       struct obd_client_handle *och)
 {
-	struct ptlrpc_request *req = it->d.lustre.it_data;
+	struct ptlrpc_request *req = it->it_data;
 	struct mdt_body *body;
 
 	body = req_capsule_server_get(&req->rq_pill, &RMF_MDT_BODY);
 	och->och_fh = body->mbo_handle;
 	och->och_fid = body->mbo_fid1;
-	och->och_lease_handle.cookie = it->d.lustre.it_lock_handle;
+	och->och_lease_handle.cookie = it->it_lock_handle;
 	och->och_magic = OBD_CLIENT_HANDLE_MAGIC;
 	och->och_flags = it->it_flags;
 
@@ -519,7 +519,7 @@ int ll_file_open(struct inode *inode, struct file *file)
                 RETURN(0);
         }
 
-        if (!it || !it->d.lustre.it_disposition) {
+	if (!it || !it->it_disposition) {
                 /* Convert f_flags into access mode. We cannot use file->f_mode,
                  * because everything but O_ACCMODE mask was stripped from
                  * there */
@@ -584,7 +584,7 @@ restart:
                 }
         } else {
                 LASSERT(*och_usecount == 0);
-                if (!it->d.lustre.it_disposition) {
+		if (!it->it_disposition) {
                         /* We cannot just request lock handle now, new ELC code
                            means that one of other OPEN locks for this file
                            could be cancelled, and since blocking ast handler
@@ -627,7 +627,7 @@ restart:
 
 		LASSERTF(it_disposition(it, DISP_ENQ_OPEN_REF),
 			 "inode %p: disposition %x, status %d\n", inode,
-			 it_disposition(it, ~0), it->d.lustre.it_status);
+			 it_disposition(it, ~0), it->it_status);
 
 		rc = ll_local_open(file, it, fd, *och_p);
 		if (rc)
@@ -664,7 +664,7 @@ out_openerr:
         }
 
 	if (it && it_disposition(it, DISP_ENQ_OPEN_REF)) {
-		ptlrpc_req_finished(it->d.lustre.it_data);
+		ptlrpc_req_finished(it->it_data);
 		it_clear_disposition(it, DISP_ENQ_OPEN_REF);
 	}
 
@@ -799,12 +799,12 @@ ll_lease_open(struct inode *inode, struct file *file, fmode_t fmode,
 
 	/* already get lease, handle lease lock */
 	ll_set_lock_data(sbi->ll_md_exp, inode, &it, NULL);
-	if (it.d.lustre.it_lock_mode == 0 ||
-	    it.d.lustre.it_lock_bits != MDS_INODELOCK_OPEN) {
+	if (it.it_lock_mode == 0 ||
+	    it.it_lock_bits != MDS_INODELOCK_OPEN) {
 		/* open lock must return for lease */
 		CERROR(DFID "lease granted but no open lock, %d/"LPU64".\n",
-			PFID(ll_inode2fid(inode)), it.d.lustre.it_lock_mode,
-			it.d.lustre.it_lock_bits);
+			PFID(ll_inode2fid(inode)), it.it_lock_mode,
+			it.it_lock_bits);
 		GOTO(out_close, rc = -EPROTO);
 	}
 
@@ -813,10 +813,10 @@ ll_lease_open(struct inode *inode, struct file *file, fmode_t fmode,
 
 out_close:
 	/* Cancel open lock */
-	if (it.d.lustre.it_lock_mode != 0) {
+	if (it.it_lock_mode != 0) {
 		ldlm_lock_decref_and_cancel(&och->och_lease_handle,
-					    it.d.lustre.it_lock_mode);
-		it.d.lustre.it_lock_mode = 0;
+					    it.it_lock_mode);
+		it.it_lock_mode = 0;
 		och->och_lease_handle.cookie = 0ULL;
 	}
 	rc2 = ll_close_inode_openhandle(inode, och, 0, NULL);
@@ -1746,7 +1746,7 @@ int ll_release_openhandle(struct dentry *dentry, struct lookup_intent *it)
 out:
 	/* this one is in place of ll_file_open */
 	if (it_disposition(it, DISP_ENQ_OPEN_REF)) {
-		ptlrpc_req_finished(it->d.lustre.it_data);
+		ptlrpc_req_finished(it->it_data);
 		it_clear_disposition(it, DISP_ENQ_OPEN_REF);
 	}
 	RETURN(rc);
@@ -4119,14 +4119,14 @@ again:
 			  PFID(&lli->lli_fid), inode);
 
 	rc = md_enqueue(sbi->ll_md_exp, &einfo, NULL, &it, op_data, &lockh, 0);
-	if (it.d.lustre.it_data != NULL)
-		ptlrpc_req_finished(it.d.lustre.it_data);
-	it.d.lustre.it_data = NULL;
+	if (it.it_data != NULL)
+		ptlrpc_req_finished(it.it_data);
+	it.it_data = NULL;
 
 	ll_finish_md_op_data(op_data);
 
-	mode = it.d.lustre.it_lock_mode;
-	it.d.lustre.it_lock_mode = 0;
+	mode = it.it_lock_mode;
+	it.it_lock_mode = 0;
 	ll_intent_drop_lock(&it);
 
 	if (rc == 0) {

lustre/include/lustre_intent.h
@@ -38,40 +38,35 @@
 #define LUSTRE_INTENT_H
 
 /* intent IT_XXX are defined in lustre/include/obd.h */
-struct lustre_intent_data {
-	int		it_disposition;
-	int		it_status;
-	__u64		it_lock_handle;
-	__u64		it_lock_bits;
-	int		it_lock_mode;
-	int		it_remote_lock_mode;
-	__u64           it_remote_lock_handle;
-	void           *it_data;
-	unsigned int    it_lock_set:1;
-};
 
 struct lookup_intent {
-	int     it_op;
-	int     it_create_mode;
-	__u64   it_flags;
-	union {
-		struct lustre_intent_data lustre;
-	} d;
+	int			 it_op;
+	int			 it_create_mode;
+	__u64			 it_flags;
+	int			 it_disposition;
+	int			 it_status;
+	__u64			 it_lock_handle;
+	__u64			 it_lock_bits;
+	int			 it_lock_mode;
+	int			 it_remote_lock_mode;
+	__u64			 it_remote_lock_handle;
+	void			*it_data;
+	unsigned int		 it_lock_set:1;
 };
 
 static inline int it_disposition(const struct lookup_intent *it, int flag)
 {
-	return it->d.lustre.it_disposition & flag;
+	return it->it_disposition & flag;
 }
 
 static inline void it_set_disposition(struct lookup_intent *it, int flag)
 {
-	it->d.lustre.it_disposition |= flag;
+	it->it_disposition |= flag;
 }
 
 static inline void it_clear_disposition(struct lookup_intent *it, int flag)
 {
-	it->d.lustre.it_disposition &= ~flag;
+	it->it_disposition &= ~flag;
 }
 
 #endif

lustre/llite/dcache.c
@@ -203,26 +203,26 @@ int ll_d_init(struct dentry *de)
 
 void ll_intent_drop_lock(struct lookup_intent *it)
 {
-        if (it->it_op && it->d.lustre.it_lock_mode) {
+	if (it->it_op && it->it_lock_mode) {
 		struct lustre_handle handle;
 
-		handle.cookie = it->d.lustre.it_lock_handle;
+		handle.cookie = it->it_lock_handle;
 
-                CDEBUG(D_DLMTRACE, "releasing lock with cookie "LPX64
-                       " from it %p\n", handle.cookie, it);
-                ldlm_lock_decref(&handle, it->d.lustre.it_lock_mode);
+		CDEBUG(D_DLMTRACE, "releasing lock with cookie "LPX64
+		       " from it %p\n", handle.cookie, it);
+		ldlm_lock_decref(&handle, it->it_lock_mode);
 
-                /* bug 494: intent_release may be called multiple times, from
-                 * this thread and we don't want to double-decref this lock */
-                it->d.lustre.it_lock_mode = 0;
-		if (it->d.lustre.it_remote_lock_mode != 0) {
-			handle.cookie = it->d.lustre.it_remote_lock_handle;
+		/* bug 494: intent_release may be called multiple times, from
+		 * this thread and we don't want to double-decref this lock */
+		it->it_lock_mode = 0;
+		if (it->it_remote_lock_mode != 0) {
+			handle.cookie = it->it_remote_lock_handle;
 
 			CDEBUG(D_DLMTRACE, "releasing remote lock with cookie"
 			       LPX64" from it %p\n", handle.cookie, it);
 			ldlm_lock_decref(&handle,
-					 it->d.lustre.it_remote_lock_mode);
-			it->d.lustre.it_remote_lock_mode = 0;
+					 it->it_remote_lock_mode);
+			it->it_remote_lock_mode = 0;
 		}
 	}
 }
@@ -235,14 +235,14 @@ void ll_intent_release(struct lookup_intent *it)
         ll_intent_drop_lock(it);
         /* We are still holding extra reference on a request, need to free it */
         if (it_disposition(it, DISP_ENQ_OPEN_REF))
-		ptlrpc_req_finished(it->d.lustre.it_data); /* ll_file_open */
+		ptlrpc_req_finished(it->it_data); /* ll_file_open */
 
-        if (it_disposition(it, DISP_ENQ_CREATE_REF)) /* create rec */
-                ptlrpc_req_finished(it->d.lustre.it_data);
+	if (it_disposition(it, DISP_ENQ_CREATE_REF)) /* create rec */
+		ptlrpc_req_finished(it->it_data);
 
-        it->d.lustre.it_disposition = 0;
-        it->d.lustre.it_data = NULL;
-        EXIT;
+	it->it_disposition = 0;
+	it->it_data = NULL;
+	EXIT;
 }
 
 void ll_invalidate_aliases(struct inode *inode)
@@ -301,7 +301,7 @@ void ll_lookup_finish_locks(struct lookup_intent *it, struct dentry *dentry)
         LASSERT(it != NULL);
         LASSERT(dentry != NULL);
 
-        if (it->d.lustre.it_lock_mode && dentry->d_inode != NULL) {
+	if (it->it_lock_mode && dentry->d_inode != NULL) {
                 struct inode *inode = dentry->d_inode;
                 struct ll_sb_info *sbi = ll_i2sbi(dentry->d_inode);
 

lustre/llite/llite_internal.h
@@ -1376,7 +1376,7 @@ static inline int ll_file_nolock(const struct file *file)
 static inline void ll_set_lock_data(struct obd_export *exp, struct inode *inode,
                                     struct lookup_intent *it, __u64 *bits)
 {
-	if (!it->d.lustre.it_lock_set) {
+	if (!it->it_lock_set) {
 		struct lustre_handle handle;
 
 		/* If this inode is a remote object, it will get two
@@ -1386,8 +1386,8 @@ static inline void ll_set_lock_data(struct obd_export *exp, struct inode *inode,
 		 * UPDATE|PERM lock. The inode will be attched to both
 		 * LOOKUP and PERM locks, so revoking either locks will
 		 * case the dcache being cleared */
-		if (it->d.lustre.it_remote_lock_mode) {
-			handle.cookie = it->d.lustre.it_remote_lock_handle;
+		if (it->it_remote_lock_mode) {
+			handle.cookie = it->it_remote_lock_handle;
 			CDEBUG(D_DLMTRACE, "setting l_data to inode "DFID
 			       "(%p) for remote lock "LPX64"\n",
 			       PFID(ll_inode2fid(inode)), inode,
@@ -1395,19 +1395,19 @@ static inline void ll_set_lock_data(struct obd_export *exp, struct inode *inode,
 			md_set_lock_data(exp, &handle.cookie, inode, NULL);
 		}
 
-		handle.cookie = it->d.lustre.it_lock_handle;
+		handle.cookie = it->it_lock_handle;
 
 		CDEBUG(D_DLMTRACE, "setting l_data to inode "DFID"(%p)"
 		       " for lock "LPX64"\n",
 		       PFID(ll_inode2fid(inode)), inode, handle.cookie);
 
 		md_set_lock_data(exp, &handle.cookie, inode,
-				 &it->d.lustre.it_lock_bits);
-		it->d.lustre.it_lock_set = 1;
+				 &it->it_lock_bits);
+		it->it_lock_set = 1;
 	}
 
 	if (bits != NULL)
-		*bits = it->d.lustre.it_lock_bits;
+		*bits = it->it_lock_bits;
 }
 
 static inline void ll_lock_dcache(struct inode *inode)

lustre/llite/llite_lib.c
@@ -2341,11 +2341,11 @@ int ll_prep_inode(struct inode **inode, struct ptlrpc_request *req,
 	 * 2. layout was changed by another client
 	 * 3. proc2: refresh layout and layout lock granted
 	 * 4. proc1: to apply a stale layout */
-	if (it != NULL && it->d.lustre.it_lock_mode != 0) {
+	if (it != NULL && it->it_lock_mode != 0) {
 		struct lustre_handle lockh;
 		struct ldlm_lock *lock;
 
-		lockh.cookie = it->d.lustre.it_lock_handle;
+		lockh.cookie = it->it_lock_handle;
 		lock = ldlm_handle2lock(&lockh);
 		LASSERT(lock != NULL);
 		if (ldlm_has_layout(lock)) {

lustre/llite/namei.c
@@ -470,7 +470,7 @@ static int ll_lookup_it_finish(struct ptlrpc_request *request,
 	/* NB 1 request reference will be taken away by ll_intent_lock()
 	 * when I return */
 	CDEBUG(D_DENTRY, "it %p it_disposition %x\n", it,
-	       it->d.lustre.it_disposition);
+	       it->it_disposition);
 	if (!it_disposition(it, DISP_LOOKUP_NEG)) {
                 rc = ll_prep_inode(&inode, request, (*de)->d_sb, it);
                 if (rc)
@@ -519,7 +519,7 @@ static int ll_lookup_it_finish(struct ptlrpc_request *request,
 		/* Check that parent has UPDATE lock. */
 		struct lookup_intent parent_it = {
 					.it_op = IT_GETATTR,
-					.d.lustre.it_lock_handle = 0 };
+					.it_lock_handle = 0 };
 		struct lu_fid	fid = ll_i2info(parent)->lli_fid;
 
 		/* If it is striped directory, get the real stripe parent */
@@ -859,10 +859,10 @@ static struct inode *ll_create_node(struct inode *dir, struct lookup_intent *it)
         int rc;
         ENTRY;
 
-        LASSERT(it && it->d.lustre.it_disposition);
+	LASSERT(it && it->it_disposition);
 
-        LASSERT(it_disposition(it, DISP_ENQ_CREATE_REF));
-        request = it->d.lustre.it_data;
+	LASSERT(it_disposition(it, DISP_ENQ_CREATE_REF));
+	request = it->it_data;
         it_clear_disposition(it, DISP_ENQ_CREATE_REF);
         rc = ll_prep_inode(&inode, request, dir->i_sb, it);
         if (rc)
@@ -1103,8 +1103,8 @@ static int ll_create_nd(struct inode *dir, struct dentry *dentry,
 	lld->lld_it = NULL;
 
         /* Was there an error? Propagate it! */
-        if (it->d.lustre.it_status) {
-                rc = it->d.lustre.it_status;
+	if (it->it_status) {
+		rc = it->it_status;
                 goto out;
         }
 

lustre/llite/statahead.c
@@ -592,7 +592,7 @@ static void sa_instantiate(struct ll_statahead_info *sai,
 		}
 	}
 
-        it->d.lustre.it_lock_handle = entry->se_handle;
+	it->it_lock_handle = entry->se_handle;
 	rc = md_revalidate_lock(ll_i2mdexp(dir), it, ll_inode2fid(dir), NULL);
         if (rc != 1)
                 GOTO(out, rc = -EAGAIN);
@@ -683,7 +683,7 @@ static int ll_statahead_interpret(struct ptlrpc_request *req,
 		 * thread enqueues lock on parent in readdir and another
 		 * process enqueues lock on child with parent lock held, eg.
 		 * unlink. */
-		handle = it->d.lustre.it_lock_handle;
+		handle = it->it_lock_handle;
 		ll_intent_drop_lock(it);
 	}
 
@@ -789,7 +789,7 @@ static int sa_revalidate(struct inode *dir, struct sa_entry *entry,
 {
 	struct inode *inode = dentry->d_inode;
 	struct lookup_intent it = { .it_op = IT_GETATTR,
-				    .d.lustre.it_lock_handle = 0 };
+				    .it_lock_handle = 0 };
 	struct md_enqueue_info *minfo;
 	int rc;
 	ENTRY;
@@ -804,7 +804,7 @@ static int sa_revalidate(struct inode *dir, struct sa_entry *entry,
 	rc = md_revalidate_lock(ll_i2mdexp(dir), &it, ll_inode2fid(inode),
 				NULL);
 	if (rc == 1) {
-		entry->se_handle = it.d.lustre.it_lock_handle;
+		entry->se_handle = it.it_lock_handle;
 		ll_intent_release(&it);
 		RETURN(1);
 	}
@@ -1461,7 +1461,7 @@ static int revalidate_statahead_dentry(struct inode *dir,
 	if (entry->se_state == SA_ENTRY_SUCC && entry->se_inode != NULL) {
 		struct inode *inode = entry->se_inode;
 		struct lookup_intent it = { .it_op = IT_GETATTR,
-					    .d.lustre.it_lock_handle =
+					    .it_lock_handle =
 						entry->se_handle };
 		__u64 bits;
 

lustre/llite/xattr_cache.c
@@ -337,8 +337,8 @@ static int ll_xattr_find_get_lock(struct inode *inode,
 					LCK_PR);
 		if (mode != 0) {
 			/* fake oit in mdc_revalidate_lock() manner */
-			oit->d.lustre.it_lock_handle = lockh.cookie;
-			oit->d.lustre.it_lock_mode = mode;
+			oit->it_lock_handle = lockh.cookie;
+			oit->it_lock_mode = mode;
 			goto out;
 		}
 	}
@@ -363,7 +363,7 @@ static int ll_xattr_find_get_lock(struct inode *inode,
 		RETURN(rc);
 	}
 
-	*req = (struct ptlrpc_request *)oit->d.lustre.it_data;
+	*req = (struct ptlrpc_request *)oit->it_data;
 out:
 	down_write(&lli->lli_xattrs_list_rwsem);
 	mutex_unlock(&lli->lli_xattrs_enq_lock);
@@ -410,10 +410,10 @@ static int ll_xattr_cache_refill(struct inode *inode, struct lookup_intent *oit)
 		GOTO(out_maybe_drop, rc = -EIO);
 	}
 
-	if (oit->d.lustre.it_status < 0) {
+	if (oit->it_status < 0) {
 		CDEBUG(D_CACHE, "getxattr intent returned %d for fid "DFID"\n",
-		       oit->d.lustre.it_status, PFID(ll_inode2fid(inode)));
-		rc = oit->d.lustre.it_status;
+		       oit->it_status, PFID(ll_inode2fid(inode)));
+		rc = oit->it_status;
 		/* xattr data is so large that we don't want to cache it */
 		if (rc == -ERANGE)
 			rc = -EAGAIN;
@@ -499,8 +499,8 @@ out_destroy:
 	up_write(&lli->lli_xattrs_list_rwsem);
 
 	ldlm_lock_decref_and_cancel((struct lustre_handle *)
-					&oit->d.lustre.it_lock_handle,
-					oit->d.lustre.it_lock_mode);
+					&oit->it_lock_handle,
+					oit->it_lock_mode);
 
 	goto out_no_unlock;
 }

LaudateCorpus1/lustre

commit 15189be6abcae56f2988b335aab09307a2d69f6f

LUS-9198 llite: it_lock_bits should be bit-wise tested
If lock_mode is not set then ensure that it_lock_bits has
the MDS_INODELOCK_OPEN bit set

Fixes: 5db0e2de00 ("LU-7403 llite: flatten struct lookup_intent")

Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Id964961ff0d7fff5cf592af8660b8efc3d033e03
Reviewed-on: https://es-gerrit.dev.cray.com/157676
Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
Reviewed-by: Alexey Lyashkov <c17817@cray.com>
Reviewed-by: Vitaly Fertman <c17818@cray.com>
Tested-by: Alexander Lezhoev <c17454@cray.com>

lustre/llite/file.c
@@ -1043,8 +1043,8 @@ ll_lease_open(struct inode *inode, struct file *file, fmode_t fmode,
 
 	/* already get lease, handle lease lock */
 	ll_set_lock_data(sbi->ll_md_exp, inode, &it, NULL);
-	if (it.it_lock_mode == 0 ||
-	    it.it_lock_bits != MDS_INODELOCK_OPEN) {
+	if (!it.it_lock_mode ||
+	    !(it.it_lock_bits & MDS_INODELOCK_OPEN)) {
 		/* open lock must return for lease */
 		CERROR(DFID "lease granted but no open lock, %d/%llu.\n",
 			PFID(ll_inode2fid(inode)), it.it_lock_mode,